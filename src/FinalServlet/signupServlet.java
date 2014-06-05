d62e52d078ef5a0304b2f83db87dbfe1ac475c69
ismetacar <acar.dogan@bil.omu.edu.tr>
2014-06-05 22:29:37 +0300

    passwd changed

--- src/FinalServlet/signupServlet.java
+++ src/FinalServlet/signupServlet.java
@@ -80,10 +80,7 @@ public class signupServlet extends HttpServlet {
 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
