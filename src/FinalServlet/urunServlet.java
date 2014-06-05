72061bec3ddb540f50075a20f0d371cb33c32a3a
ismetacar <acar.dogan@bil.omu.edu.tr>
2014-06-05 22:30:00 +0300

    passwd changed

--- src/FinalServlet/urunServlet.java
+++ src/FinalServlet/urunServlet.java
@@ -74,10 +74,7 @@ public class urunServlet extends HttpServlet {
 		// response.sendRedirect("anasayfa.jsp");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
