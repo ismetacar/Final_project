72d294ba9b08486512dacb490a4588c7b46c891e
ismetacar <acar.dogan@bil.omu.edu.tr>
2014-06-05 22:29:09 +0300

    passwd changed

--- src/FinalServlet/postServlet.java
+++ src/FinalServlet/postServlet.java
@@ -77,10 +77,7 @@ public class postServlet extends HttpServlet {
 		// response.sendRedirect("anasayfa.jsp");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
